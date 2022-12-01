6468bab091bd15787206504871ba7c9b4db23e6e
Dale Hawkins <dkhawk@gmail.com>
2022-12-01 11:12:21 -0700

    Day 01 and templates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,18 @@ import aoc2022.day01.appMain
 import aoc2022.day01.Day
 import kotlinx.coroutines.runBlocking
 
-fun main() {
-  // appMain()
-
-  runBlocking {
-    Day(this).apply {
-      useRealData = true
-      initialize()
-      part1()
-      part2()
+fun main(args: Array<String>) {
+  val desktopApp = args.contains("desktop")
+  if (desktopApp) {
+    appMain()
+  } else {
+    runBlocking {
+      Day(this).apply {
+        useRealData = true
+        initialize()
+        part1()
+        part2()
+      }
     }
   }
 }
