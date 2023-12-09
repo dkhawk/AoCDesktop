ebd013596d33df3a1dbb4873110d46c43c36289f
Dale Hawkins <dkhawk@gmail.com>
2023-12-09 10:40:12 -0700

    2023 day 5 part 2 -- finally!

--- src/main/kotlin/aoc2023/day05/Day.kt
+++ src/main/kotlin/aoc2023/day05/Day.kt
@@ -3,8 +3,6 @@ package aoc2023.day05
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import java.lang.Long.max
-import java.lang.Long.min
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
@@ -106,18 +104,12 @@ class Day(private val scope: CoroutineScope) {
     Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
      */
 
-    // val m = rangeMaps["fertilizer-to-water"]!!
-    // println(remap(m, 53))
-    //
-    // return
     val maps = progression.map { rangeMaps.getValue(it) }
 
     val closest = seeds.minOf {seed ->
-      // print("$seed -> ")
       val location = maps.fold(seed) { value, map ->
-        remap(map, value) // .also { print("$it, ") }
+        remap(map, value)
       }
-      // println()
       location
     }
 
@@ -146,8 +138,7 @@ class Day(private val scope: CoroutineScope) {
   }
 
   private fun parseSeeds(input1: List<String>): List<Long> {
-    val seeds = input1.first().split(": ").last().split(' ').map { it.trim().toLong() }
-    return seeds
+    return input1.first().split(": ").last().split(' ').map { it.trim().toLong() }
   }
 
   private fun remap(map: List<RangeMap>, value: Long): Long {
@@ -159,97 +150,76 @@ class Day(private val scope: CoroutineScope) {
   }
 
   fun part2() {
-    // println((0L..10L).splitBy(11L..20L))
-    // println((11L..20L).splitBy(0L..10L))
-    // println((0L..10L).splitBy(5L..15L))
-    // println((5L..7L).splitBy(0L..10L))
-    // showSplit(0L..10L, 11L..20L)
-    // showSplit(11L..20L, 0L..10L)
-    // showSplit(0L..10L, 5L..15L)
-    // showSplit(5L..7L, 0L..10L)
-    // showSplit(5L..15L, 0L..10L)
-    //
-    // return
-
-
     val seeds = parseSeeds(input)
     buildRangeMaps(input)
 
-    // rangeMaps.forEach { (key, value) ->
-    //   println(key)
-    //   println(value.joinToString("\n"))
-    //   println()
-    // }
-
-    val mappers = progression.map { rangeMaps.getValue(it) }.map { item -> item.map {MapperRanges(it.toSourceRange(), it.toDestinationRange()) } }
-
-    println(seeds)
+    val stages = progression.map { rangeMaps.getValue(it) }.map {
+        item -> item.map {MapperRanges(it.toSourceRange(), it.toDestinationRange()) }
+    }
 
     // Convert the seeds to ranges
     val seedRanges = seeds.windowed(2, 2, false).map {
       it.first() until (it.first() + it.last())
     }
-    println(seedRanges)
 
-    val remapped = mappers.fold(seedRanges) { rangesToRemap, mapper ->
-      rangesToRemap.flatMap { it.remap(mapper) }
+    val journeys = stages.fold(
+      mutableListOf(
+        seedRanges.map { null as LongRange? to it }
+      )
+    ) { m, stage ->
+      val sourceRanges = m.last().map { it.second }
+
+      val sources = ArrayDeque(sourceRanges)
+
+      val destinations = mutableListOf<Pair<LongRange, LongRange>>()
+      while (sources.isNotEmpty()) {
+        val sourceRange = sources.removeFirst()
+        val remappings = mutableListOf<Pair<LongRange, LongRange>>()
+
+        val sourceFirst = sourceRange.first
+        val sourceLast = sourceRange.last
+
+        val mapper = stage.firstOrNull { sourceFirst in it.source }
+        if (mapper == null) {
+          val splitter = stage.firstOrNull { sourceLast in it.source }
+          if (splitter != null) {
+            // an unmapped range
+            val range = sourceFirst until splitter.source.first
+            remappings.add(range to range)
+            // But we now have another range to process
+            val newSource = splitter.source.first..sourceLast
+            sources.add(newSource)
+          } else {
+            remappings.add(sourceRange to sourceRange)
+          }
+        } else {
+          if (sourceLast in mapper.source) {
+            val destinationStart = mapper.remapValue(sourceFirst)
+            val destinationEnd = mapper.remapValue(sourceLast)
+            val destination = destinationStart..destinationEnd
+            remappings.add(
+              sourceRange to destination
+            )
+          } else {
+            val destinationStart = mapper.remapValue(sourceFirst)
+            val destinationEnd = mapper.destination.last
+            remappings.add(
+              sourceRange.first..mapper.source.last to destinationStart..destinationEnd
+            )
+
+            val newSource = (mapper.source.last + 1)..sourceRange.last
+            sources.add(newSource)
+          }
+        }
+
+        destinations.addAll(remappings)
+      }
+
+      m.apply { add(destinations) }
     }
 
-    println(remapped)
-
-    return
-
-    // map from the seedRanges to the first mapper source ranges
-    val result = seedRanges.map { seedRange ->
-      println("---------")
-      seedRange.remap(mappers.first()).also { println(it.joinToString("\n")) }
-      //
-      // val sr = r1.map { it.source }.map {
-      //   val answer = seedRange.splitBy(it)
-      //   println("$seedRange split by $it is $answer")
-      //   answer
-      // }
-      // println(sr.joinToString("\n"))
-      // sr
-    }
-
-    println("======================")
-
-    println(result.joinToString("\n"))
-
-    /*
-    Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
-    Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
-    Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
-    Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
-     */
-
-    // val m = rangeMaps["fertilizer-to-water"]!!
-    // println(remap(m, 53))
-    //
-    // return
-    // val maps = progression.reversed().map { rangeMaps.getValue(it) }
-    //
-    // val goal = 0
-    //
-    // val closest = seeds.minOf {seed ->
-    //   // print("$seed -> ")
-    //   val location = maps.fold(seed) { value, map ->
-    //     remap(map, value) // .also { print("$it, ") }
-    //   }
-    //   // println()
-    //   location
-    // }
-    //
-    // println(closest)
-  }
-
-  private fun showSplit(
-    rangeToSplit: LongRange,
-    splittingRange: LongRange,
-  ) {
-    val newRanges = rangeToSplit.splitBy(splittingRange)
-    println("$rangeToSplit split by $splittingRange => $newRanges")
+    val answer = journeys.last().minOf { it.second.first }
+    println(answer)
   }
 
   fun execute() {
@@ -279,73 +249,5 @@ class Day(private val scope: CoroutineScope) {
   }
 
   companion object {
-    val cameFrom = mutableMapOf<LongRange, LongRange>()
   }
-}
-
-private fun LongRange.remap(mappers: List<Day.MapperRanges>): List<LongRange> {
-  val remapped = mutableListOf<LongRange>()
-  val unmapped = ArrayDeque<LongRange>(100)
-
-  unmapped.add(this)
-
-  val mapperIter = mappers.iterator()
-
-  while (unmapped.isNotEmpty() && mapperIter.hasNext()) {
-    val range = unmapped.removeFirst()
-    val mapper = mapperIter.next()
-
-    if (range.last < mapper.source.first || range.first > mapper.source.last) {
-      // total miss
-      unmapped.add(range)
-    } else {
-      // Unmapped before
-      if (range.first < mapper.source.first)
-        unmapped.add(range.first until mapper.source.first)
-      // Unmapped after
-      if (range.last > mapper.source.last)
-        unmapped.add((mapper.source.last + 1) .. range.last)
-
-      // Now remap the overlapping parts
-      val start = max(range.first, mapper.source.first)
-      val end = min(range.last, mapper.source.last)
-
-      val newRange = mapper.remapValue(start)..mapper.remapValue(end)
-      remapped.add(newRange)
-      Day.cameFrom[newRange] = range
-    }
-  }
-
-  remapped.addAll(unmapped)
-
-  return remapped.toList()
-}
-
-private fun LongRange.splitBy(other: LongRange): MutableList<LongRange> {
-  val result = mutableListOf<LongRange>()
-  if (this.last < other.first) {
-    result.add(this)
-  }
-
-  if (this.first > other.last) {
-    result.add(this)
-  }
-
-  if (this.first <= other.first) {
-    if (this.last in other) {
-      result.add(this.first until other.first)
-      result.add(other.first .. this.last)
-    }
-  }
-
-  if (this.first in other && this.last in other) {
-    result.add(this)
-  }
-
-  if (this.first in other && this.last > other.last) {
-    result.add(first..other.last)
-    result.add((other.last + 1)..last)
-  }
-
-  return result
 }
